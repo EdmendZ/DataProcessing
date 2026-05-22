20357ae7413dd1c172d9856ac2f9c0fbf406048a
Zephyr <edmendzhang214@gmail.com>
2026-05-22 20:40:46 +0800

    NumpyAndPandas

---
+++ housedata.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    @Author  : Zephyr
+    @Date    : 2026/5/22 16:25
+    @Project  : NumpyAndPandas
+    @File     : housedata.py
+    @IDE      : PyCharm
+    @Description: 
+"""
